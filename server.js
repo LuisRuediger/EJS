d76d3d628c5089f3a796fd069a9a9803a2b21479
Luis Ruediger <luisruediger27@gmail.com>
2022-02-15 14:34:56 -0300

    add page about

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ app.set("view engine", "ejs");
 app.get("/", function (req, res) {
   res.render("index");
 });
+app.get("/sobre", function (req, res) {
+  res.render("about");
+});
 
 app.listen(8080);
 console.log("Server iniciado");
